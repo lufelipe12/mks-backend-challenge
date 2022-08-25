4fc0b6d4337a22156437afc5ccd976d3719a3d27
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-08-25 09:53:06 -0300

    feat: added jwt strategy apllying to apply on movie roytes

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModel } from 'src/models/user.model';
 import { AuthService } from '../services/sessions/auth.service';
 import { LocalStrategy } from 'src/strategies/local.strategy';
+import { JwtStategy } from 'src/strategies/jwt.strategy';
 import { jwtConstants } from 'src/constants/constants';
 import { UsersService } from 'src/services/users/users.service';
 import { SessionsController } from 'src/controllers/session.controller';
@@ -19,7 +20,7 @@ import { SessionsController } from 'src/controllers/session.controller';
     }),
   ],
   controllers: [SessionsController],
-  providers: [AuthService, LocalStrategy, UsersService],
-  exports: [AuthService, LocalStrategy, UsersService],
+  providers: [AuthService, LocalStrategy, JwtStategy, UsersService],
+  exports: [AuthService, LocalStrategy, JwtStategy, UsersService],
 })
 export class AuthModule {}
