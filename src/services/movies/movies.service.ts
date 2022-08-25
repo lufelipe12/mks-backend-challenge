0a1aa607b30e04e694064a220e081bc02736b227
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-08-25 10:00:54 -0300

    fix: fix conflicts

--- src/services/movies/movies.service.ts
+++ src/services/movies/movies.service.ts
@@ -1,4 +1,18 @@
+<<<<<<< Updated upstream
 import { Injectable } from '@nestjs/common';
+=======
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { MovieModel } from 'src/models/movie.model';
+import { MovieSchema } from 'src/schemas/movie.schema';
+>>>>>>> Stashed changes
 
 @Injectable()
 export class MoviesService {}
